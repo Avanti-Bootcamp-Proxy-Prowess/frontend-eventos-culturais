5dba0e8ebcf47bb1c02f11bdc7ace0195f228cd1
Quézia Filadelfo <queziafiladelfo@gmail.com>
2024-05-19 00:25:40 -0300

    #3 feat: Add commit anterior

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+export function Login() {
+
+    return (
+        <Container>
+            <h1>LOGIN AQUI</h1>
+        </Container>
+    )
+}
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+export function SignUp() {
+
+    return (
+        <Container>
+            <h1>CADASTRO AQUI AQUI</h1>
+        </Container>
+    )
+}
