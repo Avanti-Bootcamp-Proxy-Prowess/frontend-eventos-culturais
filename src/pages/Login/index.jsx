0d751fdfcd39e3d5a23c21a05b12675a8bc4546d
Quézia Filadelfo <queziafiladelfo@gmail.com>
2024-05-19 00:45:02 -0300

    #2 feat: Adiciona tela de login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,35 @@
-import { Container } from "./styles";
+import { FiMail, FiLock } from "react-icons/fi";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { Container, Form, Background } from "./styles";
+
+import { Link } from "react-router-dom";
 
 export function Login() {
-
     return (
         <Container>
-            <h1>LOGIN AQUI</h1>
+            <Form>
+                <h1>Rocket Events</h1>
+                <p>Aplicação para gerenciamento de eventos culturais.</p>
+                <h2>Faça seu login</h2>
+                <Input
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+
+                <Input
+                    placeholder="Senha"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Button title="Entrar" />
+                <Link to={"/register"}>
+                    Criar conta
+                </Link>
+            </Form>
+            <Background />
         </Container>
-    )
+    );
 }
