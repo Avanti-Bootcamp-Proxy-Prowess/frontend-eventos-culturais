6bb1fc629bc2c976f7d08e246f3054c80504729f
Quézia Filadelfo <queziafiladelfo@gmail.com>
2024-05-21 23:27:25 -0300

    #8 feat: Integração de usuario
    - Guarda informações de login no local storage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../services/api";
 
@@ -6,12 +6,15 @@ export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
     const [data, setData] = useState({});
-    
+
     async function login({ email, password }) {
 
         try {
             const response = await api.post("/login", { email, password });
             const { user, token } = response.data;
+
+            localStorage.setItem("@rocketevents:user", JSON.stringify(user));
+            localStorage.setItem("@rocketevents:token", token);
 
             api.defaults.headers.authorization = `Bearer ${token}`;
             setData({ user, token })
@@ -26,8 +29,21 @@ function AuthProvider({ children }) {
         }
     }
 
+    useEffect(() => {
+        const token = localStorage.getItem("@rocketevents:token");
+        const user = localStorage.getItem("@rocketevents:user");
+
+        if(token && user) {
+            api.defaults.headers.authorization = `Bearer ${token}`;
+            setData({
+                token,
+                user: JSON.parse(user)
+            });
+        }
+    }, []);
+
     return (
-        <AuthContext.Provider value={{ login, data: data.user }}>
+        <AuthContext.Provider value={{ login, user: data.user }}>
             {children}
         </AuthContext.Provider>
     )
