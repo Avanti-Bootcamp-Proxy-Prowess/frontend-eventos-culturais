419dcc8098d0b59636e6d09af666096072c5e67a
yan <yanbreno27@gmail.com>
2024-05-23 22:27:03 -0300

    #20 feat: adiciona página de "Error 404"

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import { Login } from "../pages/Login"
 import { SignUp } from "../pages/SignUp";
 import { HomePage } from "../pages/HomePage";
+import { Error } from "../pages/Error";
 
 
 export function AuthRoutes() {
@@ -12,6 +13,7 @@ export function AuthRoutes() {
       <Route path="/" element={<HomePage />} />  
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<SignUp />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   )
 }
