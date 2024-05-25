848e93c7ef816a1bb8e2b9e761533719d88c135e
yan <yanbreno27@gmail.com>
2024-05-25 00:21:52 -0300

    #4 feat: adicionando novas funcionalidades
    Estilos e Imagens

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -9,7 +9,7 @@ export const Container = styled.div`
   color: ${({ theme }) => theme.COLORS.GRAY_300};
   
   margin-bottom: 8px;
-  border-radius: 10px;
+  border-radius: 8px;
 
   > input {
     height: 56px;
