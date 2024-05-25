78fd121139e35d75cb81d3b5cc61abbee8623b5d
yan <yanbreno27@gmail.com>
2024-05-24 23:57:53 -0300

    #4 Ajustes Front
    Atualização das estilizações globais.

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,7 +3,7 @@
 export default {
     COLORS: {
       BACKGROUND_900: "#001621",
-      BACKGROUND_800: "#001A27",
+      BACKGROUND_800: "#001A32",
       BACKGROUND_700: "#3E3B47",
   
       WHITE: "#F4EDE8",
@@ -12,6 +12,8 @@ export default {
       GRAY_100: "#999591",
       GRAY_300: "#666360",
   
-      RED: "#FF002E"
+      RED: "#FF002E",
+      MOONSTONE: "#21B9CB",
+      BRIGHT_ORANGE: "#FE4F6A"
     }
   }
