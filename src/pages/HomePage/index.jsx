164aeb30d276b9b6eead04126740c546ea903f8f
yan <yanbreno27@gmail.com>
2024-05-24 23:32:11 -0300

    #6 feat: Tela Homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,10 +1,32 @@
-import { Container } from "./styles";
+import { Conteiner, Header, Main } from "./styles";
+import { Button } from "../../components/Button";
+import iconRocket from "../../assets/icon-rocket.png";
+import imagePresentation from "../../assets/image-presentation.png";
 
 export function HomePage() {
 
     return (
-        <Container>
-            <h1>HOMEPAGE AQUI</h1>
-        </Container>
+        <Conteiner>
+            <Header>
+                <div className="logo">
+                    <img src={iconRocket} alt="Foguete em decolagem"/>
+                    <span>Rocket Events</span>
+                </div>
+                <div className="nav-buttons">
+                    <Button title="Login" style={{ width: "auto", height: "40px", marginTop: "0px" }}/>
+                    <Button title="Cadastro" style={{ width: "100px", height: "40px", marginTop: "0px" }}/>
+                </div>
+            </Header>
+            <Main>
+                <div className="main-text">
+                    <h1>Catalogue os seus Eventos</h1>
+                    <p>
+                        Crie, organize e participe de eventos de maneira intuitiva, conectando-se a uma comunidade de entusiastas culturais e mantendo-se informado sobre as últimas tendências em entretenimento e cultura.
+                    </p>
+                    <Button title="Começar agora" style={{ width: "auto" }}/>
+                </div>
+                <img src={imagePresentation} alt="Viajante indo em direção a foguete para desbravar novas culturas"/>
+            </Main>
+        </Conteiner>
     )
 }
