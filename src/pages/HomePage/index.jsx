cf53ee473bb3a4abce377599595fbd77b4aa6e93
Quézia Filadelfo <queziafiladelfo@gmail.com>
2024-05-25 13:08:16 -0300

    #1 feat: atualização do README

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,6 +2,7 @@ import { Conteiner, Header, Main } from "./styles";
 import { Button } from "../../components/Button";
 import iconRocket from "../../assets/icon-rocket.png";
 import imagePresentation from "../../assets/image-presentation.png";
+import { Link } from "react-router-dom";
 
 //import { useAuth } from "../../hooks/auth"
 
@@ -15,8 +16,16 @@ export function HomePage() {
                     <span>Rocket Events</span>
                 </div>
                 <div className="nav-buttons">
-                    <Button title="Login" style={{ width: "auto", height: "40px", marginTop: "0px" }}/>
-                    <Button title="Cadastro" style={{ width: "100px", height: "40px", marginTop: "0px" }}/>
+                    <Link to="/login" style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <Button title={"Login"} style={{ width: "auto", height: "40px", marginTop: "0px" }}>
+                            Login
+                        </Button>
+                    </Link>
+                    <Link to="/register" style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <Button title="Cadastro" style={{ width: "100px", height: "40px", marginTop: "0px" }}>
+                            Cadastro
+                        </Button>
+                    </Link>
                 </div>
             </Header>
             <Main>
@@ -25,7 +34,11 @@ export function HomePage() {
                     <p>
                         Crie, organize e participe de eventos de maneira intuitiva, conectando-se a uma comunidade de entusiastas culturais e mantendo-se informado sobre as últimas tendências em entretenimento e cultura.
                     </p>
-                    <Button title="Começar agora" style={{ width: "auto" }}/>
+                    <Link to="/register" style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <Button title="Começar agora" style={{ width: "auto" }}>
+                            Cadastro
+                        </Button>
+                    </Link>
                 </div>
                 <img src={imagePresentation} alt="Viajante indo em direção a foguete para desbravar novas culturas"/>
             </Main>
